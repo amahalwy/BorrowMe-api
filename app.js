6717e9d0eb155191cb290681755e1622e8c5eacc
Ahmed El Mahallawy <amahalwy@gmail.com>
2021-03-11 21:11:40 -0500

    Add CORS module into express application to allow CORS requests

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const users = require("./routes/api/users");
 const postings = require("./routes/api/postings");
@@ -15,6 +16,7 @@ mongoose
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch((err) => console.log(err));
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
@@ -25,10 +27,3 @@ app.use("/api/requests", requests);
 app.use("/api/bookings", bookings);
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
-
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static("frontend/build"));
-//   app.get("/", (req, res) => {
-//     res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
-//   });
-// }
